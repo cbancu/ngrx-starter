ec082591fe4c0889f02e393693e3835587953e80
Cristian Bancu <c.bancu@criteo.com>
2019-09-14 18:06:25 +0300

    server serve assets folder

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.use('/angular-ngrx-material-starter', express.static(__dirname + '/dist'));
+app.use('/assets', express.static(__dirname + '/dist/assets'))
+
+
+app.get('/*', function(req,res) { 
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
